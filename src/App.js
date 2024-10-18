dbb39a6ccf469598897783109c0f9572bb043522
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-10-18 23:46:58 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+
+import { publicRoute } from './routes';
+import { DefaultLayout } from './layouts';
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoute.map((route) => {
+                        const Page = route.component;
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        return (
+                            <Route
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import Home from '~/pages/Home';
+import Menu from '~/pages/Menu';
+import Login from '~/pages/Login';
+
+export const publicRoute = [
+    { path: '', component: Home },
+    { path: 'Menu', component: Menu },
+    { path: 'Login', component: Login, layout: null },
+];
+export const privateRoute = [];
